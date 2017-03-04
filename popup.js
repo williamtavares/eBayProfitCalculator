68678405e4163541e4f9a0b9b0abc09b837fa2eb
William Tavares <william.tavares93@gmail.com>
2017-03-04 04:03:23 -0500

    - Implemented all changes for the first release

--- popup.js
+++ popup.js
@@ -1,25 +1,81 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+/**
+ * Add your Analytics tracking ID here.
+ */
+var _AnalyticsCode = 'UA-39802197-9';
+/**
+ * Below is a modified version of the Google Analytics asynchronous tracking
+ * code snippet.  It has been modified to pull the HTTPS version of ga.js
+ * instead of the default HTTP version.  It is recommended that you use this
+ * snippet instead of the standard tracking snippet provided when setting up
+ * a Google Analytics account.
+ */
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', _AnalyticsCode]);
+_gaq.push(['_trackPageview']);
+(function() {
+  var ga = document.createElement('script');
+  ga.type = 'text/javascript';
+  ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(ga, s);
+})();
+/**
+ * Track a click on a button using the asynchronous tracking API.
+ *
+ * See http://code.google.com/apis/analytics/docs/tracking/asyncTracking.html
+ * for information on how to use the asynchronous tracking API.
+ */
+function trackButtonClick(e) {
+  _gaq.push(['_trackEvent', e.target.id, 'clicked']);
+}
+
+
 $(document).ready(function() {
     $("input#paypal-fee").val("2.9");
     $("input").keyup(function() {
-        var sellingPrice = parseFloat($("input#selling-price").val());
-        console.log(sellingPrice);
-        var sellingFee = parseFloat($("input#selling-fee").val());
-        console.log(sellingFee);
-        var calculatedSellingFee = (100/sellingPrice) * sellingFee;
-        console.log(calculatedSellingFee);
-        var paypalFee = parseFloat($("input#paypal-fee").val());
-        console.log(paypalFee);
-        var calculatePaypalFee = ((100/sellingPrice) * paypalFee) + .3;
-        console.log("calculatePaypalFee" + calculatePaypalFee);
-        var shipping = parseFloat($("input#shipping").val());
-        console.log(shipping);
-        var quantity = parseFloat($("input#quantity").val());
-        console.log(quantity);
+    var originalCost = 0,
+        sellingPrice = 0,
+        sellingFee = 0,
+        calculatedSellingFee = 0,
+        paypalFee = 0,
+        calculatePaypalFee = 0,
+        shipping = 0,
+        quantity = 0,
+        profit = 0;
 
-        var profit = (sellingPrice - (calculatedSellingFee + calculatePaypalFee) - shipping) * quantity;
+        originalCost = parseFloat($("input#original-cost").val());
+
+        sellingPrice = parseFloat($("input#selling-price").val());
+
+        sellingFee = parseFloat($("input#selling-fee").val());
+        
+        if(sellingFee > 0) {
+            calculatedSellingFee = sellingPrice * (sellingFee/100);
+        }
+
+        paypalFee = parseFloat($("input#paypal-fee").val());
+
+        if(paypalFee > 0) {
+            calculatePaypalFee = (sellingPrice * (paypalFee/100)) + .3;
+        }
+
+        shipping = parseFloat($("input#shipping").val());
+
+        quantity = parseFloat($("input#quantity").val());
+
+        profit = parseFloat(((sellingPrice - (calculatedSellingFee + calculatePaypalFee) - shipping) - originalCost) * quantity).toFixed(2);
 
         if(!isNaN(profit)) {
-            $("p#profit").text(profit);
+            if(profit < 0) {
+               $("p#profit").css({color: 'darkred'}) ;
+            } else {
+               $("p#profit").css({color: '#016f01'}) ;
+            }
+            $("p#profit").text('$'+profit);
         }
     });
 });
